611da8c1f4fcb63e387dba7b247565317a79d68d
Prolix <51418825+Prolix19@users.noreply.github.com>
2022-01-16 17:43:19 -0600

    Fix invalid player name bug

--- assets/js/game.js
+++ assets/js/game.js
@@ -191,13 +191,29 @@ var shop = function () {
   }
 };
 
+// function to set name
+var getPlayerName = function() {
+  var name = "";
+
+// ***************************************
+// ADD LOOP HERE WITH PROMPT AND CONDITION
+// ***************************************
+
+  while(name === "" || name === null) {
+    name = prompt("What is your robot's name?");
+  }
+
+  console.log("Your robot's name is " + name);
+  return name;
+};
+
 /* END GAME FUNCTIONS */
 
 /* GAME INFORMATION / VARIABLES */
 
 // player information
 var playerInfo = {
-  name: window.prompt("What is your robot's name?"),
+  name: getPlayerName(),
   health: 100,
   attack: 10,
   money: 10,
